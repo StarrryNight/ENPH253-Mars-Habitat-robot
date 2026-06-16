34f8ab63debd67ea57eeb5b2dcbdcc642ec27b9d
StarrryNight <92961466+StarrryNight@users.noreply.github.com>
2026-06-16 13:45:30 -0700

    initialize protobuffers
    /

---
+++ raspberry-pi/src/mrs_krab.py
@@ -0,0 +1,17 @@
+import shared.messages.robot_messages.proto;
+
+def setup():
+    return 0
+
+def super_loop():
+    while (True):
+        return 0
+
+
+
+if __name__ == '__main__':
+    setup()
+    try:
+        super_loop()
+    except KeyboardInterrupt:
+        print("Loop terminated.")
