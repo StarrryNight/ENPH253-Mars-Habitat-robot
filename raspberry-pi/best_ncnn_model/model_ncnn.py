0d0de2edf6c966682ab082d1bac4c22db8af1473
Samuel Ubuntu Laptop <lausingsamuel@gmail.com>
2026-07-27 12:44:48 -0700

    update pi

--- raspberry-pi/best_ncnn_model/model_ncnn.py
+++ raspberry-pi/best_ncnn_model/model_ncnn.py
@@ -4,12 +4,12 @@ import torch
 
 def test_inference():
     torch.manual_seed(0)
-    in0 = torch.rand(1, 3, 640, 640, dtype=torch.float)
+    in0 = torch.rand(1, 3, 320, 320, dtype=torch.float)
     out = []
 
     with ncnn.Net() as net:
-        net.load_param("/var/tmp/tmpmk8i9784/best_ncnn_model/model.ncnn.param")
-        net.load_model("/var/tmp/tmpmk8i9784/best_ncnn_model/model.ncnn.bin")
+        net.load_param("/var/tmp/tmpo4p1xaie/best_ncnn_model/model.ncnn.param")
+        net.load_model("/var/tmp/tmpo4p1xaie/best_ncnn_model/model.ncnn.bin")
 
         with net.create_extractor() as ex:
             ex.input("in0", ncnn.Mat(in0.squeeze(0).numpy()).clone())
