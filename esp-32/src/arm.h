4e1958585c889eb8818596a5980a6baf7273bf52
iAmProbablyMe <ajamshidian22@gmail.com>
2026-07-27 11:55:48 -0700

    inverse_kin

--- esp-32/src/arm.h
+++ esp-32/src/arm.h
@@ -12,6 +12,17 @@ struct ArmPose{
 	double claw_servo_degrees;
 };
 
+/*
+(x_pos=0, y_pos=0) := back shoulder joint
+(x_pos, y_pos) := front of claw body
+*/
+struct ArmCoordinate{
+	double x_pos;
+	double y_pos;
+	double wrist_yaw_servo_degrees;
+	double claw_servo_degrees;
+};
+
 class Arm
 {
 
@@ -21,6 +32,8 @@ public:
 	static constexpr double WRIST_PLACE = 35;
 	static constexpr double CLAW_OPEN = 50;
 	static constexpr double CLAW_CLOSE = 0;
+	static constexpr double UPPERARM_LENGTH = 0.2;
+	static constexpr double FOREARM_LENGTH = 0.15;
 	Arm();
 
 	// Binds the SCServo bus to Serial2 and drives the arm to its home pose.
@@ -29,6 +42,10 @@ public:
 	void begin();
 
 	void setPose(ArmPose pose);
+	void setPoseXY(ArmCoordinate pose);
+	ArmPose coordinateToDegrees(ArmCoordinate pose);
+
+
 
 private:
 	int ServoPositionConversion(double degrees);
@@ -39,6 +56,6 @@ private:
 	Servo claw_servo_;
 
 	ArmPose current_pose_;
-
+	ArmCoordinate current_poseXY_;
 
 };
